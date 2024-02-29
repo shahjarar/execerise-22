c7337a632754710cc91c48aad60c20295f02862c
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 17:59:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Task 22
+// intentional Error: if you haven't received an array index error in one of your
+// programs yet, try to make one happen. change an index in one of ypur programs
+// produce an index error. make sure ypu correct the error before closing the program.
+var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+console.log(days[6]);
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Task 22
+// intentional Error: if you haven't received an array index error in one of your
+// programs yet, try to make one happen. change an index in one of ypur programs
+// produce an index error. make sure ypu correct the error before closing the program.
+
+const days : string [] =["sunday","monday","tuesday","wednesday","thursday","friday","saturday"]
+console.log(days[6])
